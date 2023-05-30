184d94acfa9657a38bc4d739c7717e6b29b004ea
aritzie <aritie@aritzie.es>
2023-05-30 19:27:05 +0200

    first step msw

---
+++ src/mocks/handlers.js
@@ -0,0 +1,32 @@
+import { rest } from "msw";
+
+export const handlers = [
+  // Handles a GET /gifs request
+  rest.get("/gifs", (req, res, ctx) => {
+    const gifs = [
+      {
+        title: "firstGif",
+        autor: "autor1",
+        src: "srcGif1",
+        tags: ["firstTag", "secondTag"],
+        likes: 20,
+      },
+      {
+        title: "secondGif",
+        autor: "autor1",
+        src: "srcGif2",
+        tags: ["firstTag", "secondTag"],
+        likes: 10,
+      },
+      {
+        title: "thirdGif",
+        autor: "autor2",
+        src: "srcGif3",
+        tags: ["secondTag"],
+        likes: 2,
+      },
+    ];
+
+    return res(ctx.status(200), ctx.json(gifs));
+  }),
+];
