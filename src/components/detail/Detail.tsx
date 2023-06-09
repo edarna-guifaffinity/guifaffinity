30050f68ab15e2d2ff0a6ad910e231723fe01288
Edison Mejia <atllion@hotmail.com>
2023-06-09 19:20:29 +0200

    Call API on Detail

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Detail.css";
 import { Gif } from "../../models/gifs.model";
 
 export const Detail = () => {
   const [gif, setGif] = useState<Gif | undefined>(undefined);
+
+  useEffect(() => {
+    const loadGif = async () => {
+      const response = await fetch("http://localhost:3000/gifs/1");
+      const res = await response.json();
+      console.log(res);
+      setGif(res);
+    };
+    loadGif();
+  }, []);
 
   if (gif === undefined) {
     return <div>cargando ...</div>;
