6f2963631dcc4a03464f269b7ba91974faa20ee1
Edison Mejia <atllion@hotmail.com>
2023-06-09 19:14:53 +0200

    Handler get one gif

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,6 +10,69 @@ import {
 } from "msw";
 import { Gif } from "../models/gifs.model";
 
+export const gifs: Gif[] = [
+  {
+    anAlternativeText: "texto alternativo de la imagen 1",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 2",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 3",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 4",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 5",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 6",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 7",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 8",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 9",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 10",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 11",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 12",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 13",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 14",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+  {
+    anAlternativeText: "texto alternativo de la imagen 15",
+    src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+  },
+];
+
 export const handlers: RestHandler<MockedRequest<DefaultBodyType>>[] = [
   rest.get(
     "http://localhost:3000/gifs",
@@ -18,70 +81,23 @@ export const handlers: RestHandler<MockedRequest<DefaultBodyType>>[] = [
       res: ResponseComposition<DefaultBodyType>,
       ctx: RestContext
     ) => {
-      const gifs: Gif[] = [
-        {
-          anAlternativeText: "texto alternativo de la imagen 1",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 2",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 3",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 4",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 5",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 6",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 7",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 8",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 9",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 10",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 11",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 12",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 13",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 14",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-        {
-          anAlternativeText: "texto alternativo de la imagen 15",
-          src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
-        },
-      ];
-
       return res(ctx.status(200), ctx.json(gifs));
+    }
+  ),
+  rest.get(
+    "http://localhost:3000/gifs/1",
+    (
+      req: RestRequest<never, PathParams<string>>,
+      res: ResponseComposition<DefaultBodyType>,
+      ctx: RestContext
+    ) => {
+      const gif: Gif = {
+        anAlternativeText: "texto alternativo de la imagen",
+        src: "https://media.tenor.com/TpWjyNO6wz8AAAAi/banana-dance.gif",
+        title: "titulo 2",
+        author: "Pep",
+      };
+      return res(ctx.status(200), ctx.json(gif));
     }
   ),
 ];
