b0e35b4c7bbdc131161b23be241eb9498ad79a78
Nacho de M <nachodmv@outlook.es>
2023-06-13 17:34:19 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { Detail } from "./components/detail/Detail";
+import { HomeList } from "./components/home-list/HomeList";
 
 function App() {
   return (
+    <>
     <Detail />
+    <HomeList />
+    </>
     /*
     <div className="app-container">
       <section className="gifs"></section>
