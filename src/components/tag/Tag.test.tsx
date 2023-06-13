2655c8aaa6402f49b7ac7088c1ecf96aefb7fe22
Nacho de M <nachodmv@outlook.es>
2023-06-13 18:05:09 +0200

    Refactor tag v2

--- src/components/tag/Tag.test.tsx
+++ src/components/tag/Tag.test.tsx
@@ -1,14 +1,9 @@
-import { rest } from "msw";
-import { server } from "../../mocks/server";
 import { render, screen } from "@testing-library/react";
 import { Tag } from "./Tag";
 
 test("se muestra un gif", () => {
-
   render(<Tag />);
-
   expect(
     screen.getByText("#banana")
   ).toBeVisible();
-
 });
