eb31fa56b856c3a709ee78bf0c5911f488506098
aritzie <aritie@aritzie.es>
2023-07-26 18:01:50 +0200

    sacada a una constante la url base de la llamada al api

--- src/infrastructure/GifRepository.ts
+++ src/infrastructure/GifRepository.ts
@@ -1,14 +1,16 @@
 import { GifRepository } from "../domain/repositories/GifRepository";
 import { Gif } from "../models/gif.model";
 
+const apiUrl = "http://localhost:3000/api";
+
 export const GifRepositoryImpl: GifRepository = {
   getGifDetail: async (gifId: string) => {
-    const response = await fetch(`http://localhost:3000/api/gifs/${gifId}`);
+    const response = await fetch(`${apiUrl}/gifs/${gifId}`);
     const res = await response.json();
     return res as Gif;
   },
   getGifs: async () => {
-    const response = await fetch("http://localhost:3000/api/gifs");
+    const response = await fetch(apiUrl + "/gifs");
     const res = await response.json();
     return res as Gif[];
   },
